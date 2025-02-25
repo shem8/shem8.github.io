357a1b64293f5bc7969b6238bd706df79cdf716c
Shem <smagnezi8@gmail.com>
2025-02-25 12:41:20 +0000

    Site updated at 2025-02-25 12:41:20 UTC

---
+++ javascripts/advertisements-middle.js
@@ -0,0 +1,10 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Middle post -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5508892730714185"
+     data-ad-slot="8208560155"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
